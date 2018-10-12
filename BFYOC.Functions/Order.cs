d8f77486929f93a642ec7df4c57c4c6ec49b8c5b
David Chapdelaine <david.chapdelaine@gmail.com>
2018-10-11 22:58:32 -0400

    now properly processing orders

--- BFYOC.Functions/Order.cs
+++ BFYOC.Functions/Order.cs
@@ -8,5 +8,24 @@ namespace BFYOC.Functions
 {
     public class Order
     {
+        public string dateTime { get; set; }
+        public List<OrderLineItem> lineItems { get; set; }
+        public string locationId { get; set; }
+        public string locationName { get; set; }
+        public string locationPostCode { get; set; }
+        public string poNumber { get; set; }
+        public double totalCost { get; set; }
+        public double totalTax { get; set; }
+    }
+
+    public class OrderLineItem
+    {
+        public string productDescription { get; set; }
+        public string productId { get; set; }
+        public string productName { get; set; }
+        public int quantity { get; set; }
+        public double totalCost { get; set; }
+        public double totalTax { get; set; }
+        public double unitCost { get; set; }
     }
 }
--- BFYOC.Functions/OrderProcessing.cs
+++ BFYOC.Functions/OrderProcessing.cs
@@ -1,17 +1,21 @@
 // This is the default URL for triggering event grid function in the local environment.
 // http://localhost:7071/admin/extensions/EventGridExtensionConfig?functionName={functionname}
 
+using CsvHelper;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using System.Threading.Tasks;
-using Microsoft.WindowsAzure.Storage;
 using System;
+using System.Collections.Generic;
 using System.IO;
-using Microsoft.WindowsAzure.Storage.Blob;
-using CsvHelper;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace BFYOC.Functions
 {
@@ -20,15 +24,12 @@ namespace BFYOC.Functions
         private static readonly CloudStorageAccount _functionStorageAccount = CloudStorageAccount.Parse(System.Environment.GetEnvironmentVariable("AzureWebJobsStorage"));
         private static readonly CloudStorageAccount _orderStorageAccount = CloudStorageAccount.Parse(System.Environment.GetEnvironmentVariable("OrdersStorageConnection"));
 
-        public static async Task<CsvReader> GetCsvReader(this ICloudBlob blob)
+        public static async Task<CsvReader> GetCsvReader(this Task<ICloudBlob> blob)
         {
-            using (Stream stream = await blob.OpenReadAsync())
-            using (StreamReader sr = new StreamReader(stream))
-            {
-                var csvReader = new CsvReader(sr);
-                csvReader.Read();
-                return csvReader;
-            }
+            await blob;
+            Stream stream = await blob.Result.OpenReadAsync();
+            StreamReader sr = new StreamReader(stream);
+            return new CsvReader(sr);
         }
 
         public static async Task<bool> IsLeader(string instanceId)
@@ -44,7 +45,7 @@ namespace BFYOC.Functions
 
             try
             {
-                var result = await blobRef.AcquireLeaseAsync(TimeSpan.FromMinutes(60));
+                var result = await blobRef.AcquireLeaseAsync(TimeSpan.FromSeconds(30));
                 return true;
             }
             catch
@@ -60,10 +61,119 @@ namespace BFYOC.Functions
         {
             log.Info(eventGridEvent.ToString());
             var blobUrl = (string)eventGridEvent["data"]["url"];
-            var blobName = blobUrl.Substring(blobUrl.LastIndexOf('/') + 1);
+            await ProcessBlob(blobUrl, starter, log);
+        }
 
+        [FunctionName("OrderOrchestrator")]
+        public static async Task OrderOrchestrator([OrchestrationTrigger] DurableOrchestrationContext context, TraceWriter log)
+        {
+            log.Info($"Starting to wait InstanceId:{context.InstanceId}, Input:{context.GetInput<string>()}");
+
+            var orderHeaderDetailsEvent = context.WaitForExternalEvent<string>("OrderHeaderDetails");
+            var orderLineItemsEvent = context.WaitForExternalEvent<string>("OrderLineItems");
+            var productInformationEvent = context.WaitForExternalEvent<string>("ProductInformation");
+
+            await Task.WhenAll(orderHeaderDetailsEvent, orderLineItemsEvent, productInformationEvent);
+
+            var orderFiles = new OrderFiles
+            {
+                OrderHeaderDetails = orderHeaderDetailsEvent.Result,
+                OrderLineItems = orderLineItemsEvent.Result,
+                ProductInformation = productInformationEvent.Result
+            };
+
+            log.Info($"Received header:{orderFiles.OrderHeaderDetails}, line:{orderFiles.OrderLineItems}, product:{orderFiles.ProductInformation}");
+
+            var orders = await context.CallActivityAsync<IEnumerable<Order>>("OrderProcessor", orderFiles);
+
+            await context.CallActivityAsync("SaveOrders", orders);
+
+            log.Info("Work done!");
+        }
+
+        [FunctionName("OrderProcessor")]
+        public static async Task<IEnumerable<Order>> OrderProcessor([ActivityTrigger]OrderFiles orderFiles, TraceWriter log)
+        {
+            var client = _orderStorageAccount.CreateCloudBlobClient();
+            using (var orderHeaderReader = await client.GetBlobReferenceFromServerAsync(new Uri(orderFiles.OrderHeaderDetails)).GetCsvReader())
+            using (var orderLineItemsReader = await client.GetBlobReferenceFromServerAsync(new Uri(orderFiles.OrderLineItems)).GetCsvReader())
+            using (var productInformationReader = await client.GetBlobReferenceFromServerAsync(new Uri(orderFiles.ProductInformation)).GetCsvReader())
+            {
+                var orders = orderHeaderReader.GetRecords<dynamic>().ToDictionary(r => (string)r.ponumber, r => new Order
+                {
+                    dateTime = r.datetime,
+                    locationId = r.locationid,
+                    locationName = r.locationname,
+                    locationPostCode = r.locationpostcode,
+                    poNumber = r.ponumber,
+                    totalCost = double.Parse(r.totalcost),
+                    totalTax = double.Parse(r.totaltax),
+                    lineItems = new List<OrderLineItem>()
+                });
+
+                var products = productInformationReader.GetRecords<dynamic>().ToDictionary(r => (string)r.productid, r => r);
+
+                foreach (var lineItem in orderLineItemsReader.GetRecords<dynamic>())
+                {
+                    var product = products[lineItem.productid];
+                    orders[(string)lineItem.ponumber].lineItems.Add(new OrderLineItem
+                    {
+                        productId = lineItem.productid,
+                        quantity = int.Parse(lineItem.quantity),
+                        unitCost = double.Parse(lineItem.unitcost),
+                        totalCost = double.Parse(lineItem.totalcost),
+                        totalTax = double.Parse(lineItem.totaltax),
+                        productDescription = product.productdescription,
+                        productName = product.productname
+                    });
+                }
+
+                return orders.Values;
+            }
+        }
+
+        [FunctionName("ProcessingExistingBlogs")]
+        public static async Task<HttpResponseMessage> ProcessingExistingBlogs(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequestMessage req,
+            [OrchestrationClient] DurableOrchestrationClient starter,
+            TraceWriter log)
+        {
+            var client = _orderStorageAccount.CreateCloudBlobClient();
+            var containerRef = client.GetContainerReference("orders");
+            foreach (var blob in containerRef.ListBlobs())
+            {
+                await ProcessBlob(blob.Uri.ToString(), starter, log);
+            }
+
+            return req.CreateResponse(System.Net.HttpStatusCode.Accepted);
+        }
+
+        [FunctionName("SaveOrders")]
+        public static async Task SaveOrders(
+            [ActivityTrigger]IEnumerable<Order> orders,
+            [DocumentDB(databaseName: "byoc", collectionName: "orders", ConnectionStringSetting = "CosmosDBConnection")] IAsyncCollector<Order> ordersOut,
+            TraceWriter log)
+        {
+            log.Info("Saving documents");
+            await Task.WhenAll(orders.Select(o => ordersOut.AddAsync(o)));
+        }
+
+        [FunctionName("SendEvent")]
+        public static async Task<HttpResponseMessage> SendEvent(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequestMessage req,
+            [OrchestrationClient] DurableOrchestrationClient starter,
+            TraceWriter log)
+        {
+            await starter.RaiseEventAsync("20181009210400", "", "data");
+
+            return req.CreateResponse(System.Net.HttpStatusCode.Accepted);
+        }
+
+        private static async Task ProcessBlob(string blobUrl, DurableOrchestrationClient starter, TraceWriter log)
+        {
+            var blobName = blobUrl.Substring(blobUrl.LastIndexOf('/') + 1);
             var orderId = blobName.Substring(0, 14);
-            var fileType = blobName.Substring(blobName.IndexOf('-') + 1, blobName.Length - blobName.IndexOf('-') - 4);
+            var fileType = blobName.Substring(blobName.IndexOf('-') + 1, blobName.Length - blobName.IndexOf('-') - 5);
 
             var status = await starter.GetStatusAsync(orderId);
             if (status == null)
@@ -78,53 +188,15 @@ namespace BFYOC.Functions
                     log.Info("I am not the leader");
                 }
             }
-
-            log.Info($"Raising event {fileType} with value {blobUrl}");
-            await starter.RaiseEventAsync(orderId, fileType, blobUrl);
-        }
-
-        [FunctionName("OrderOrchestrator")]
-        public static async Task OrderOrchestrator([OrchestrationTrigger] DurableOrchestrationContext context, TraceWriter log)
-        {
-            log.Info($"Starting to wait Parent:{context.ParentInstanceId}, InstanceId:{context.InstanceId}, Input:{context.GetInput<string>()}");
-
-            var orderHeaderDetailsEvent = context.WaitForExternalEvent<string>("OrderHeaderDetails");
-            var orderLineItemsEvent = context.WaitForExternalEvent<string>("OrderLineItems");
-            var productInformationEvent = context.WaitForExternalEvent<string>("ProductInformation");
-
-            await Task.WhenAny(orderHeaderDetailsEvent, orderLineItemsEvent, productInformationEvent);
-
-            var orderFiles = new OrderFiles
+            else
             {
-                OrderHeaderDetails = orderHeaderDetailsEvent.Result,
-                OrderLineItems = orderLineItemsEvent.Result,
-                ProductInformation = productInformationEvent.Result
-            };
+                log.Info("I am not the leader");
+            }
 
-            log.Info($"Received header:{orderFiles.OrderHeaderDetails}, line:{orderFiles.OrderLineItems}, product:{orderFiles.ProductInformation}");
-
-            var order = await context.CallActivityAsync<Order>("OrderProcessor", orderFiles);
-        }
-
-        [FunctionName("OrderProcessor")]
-        public static async Task<Order> OrderProcessor([ActivityTrigger]OrderFiles orderFiles, TraceWriter log)
-        {
-            var client = _orderStorageAccount.CreateCloudBlobClient();
-            var OrderHeaderReader = (await client.GetBlobReferenceFromServerAsync(new Uri(orderFiles.OrderHeaderDetails))).GetCsvReader();
-            var OrderLineItemsReader = (await client.GetBlobReferenceFromServerAsync(new Uri(orderFiles.OrderLineItems))).GetCsvReader();
-            var ProductInformationReader = (await client.GetBlobReferenceFromServerAsync(new Uri(orderFiles.ProductInformation))).GetCsvReader();
-
-            return new Order();
-        }
-
-        [NoAutomaticTrigger]
-        public static void ProcessingExistingBlogs(TraceWriter log)
-        {
-            log.Info($"This is a manually triggered C# function with input");
-        }
-
-        public static void SaveOrder()
-        {
+            status = await starter.GetStatusAsync(orderId);
+            log.Info($"status is {status.RuntimeStatus}");
+            log.Info($"Raising event {fileType} with value {blobUrl} to instance {orderId}");
+            await starter.RaiseEventAsync(orderId, fileType, blobUrl);
         }
     }
 }
