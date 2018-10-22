3bd0667510153f233f68c1b9f212fe371b378f46
David Chapdelaine <david.chapdelaine@gmail.com>
2018-10-22 11:15:27 -0400

    now process sales

---
+++ BFYOC.Functions/SalesEvent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BFYOC.Functions
+{
+    public class SalesEvent
+    {
+        public DateTime dateTime { get; set; }
+        public List<SaleEventDetail> details { get; set; }
+        public string locationAddress { get; set; }
+        public string locationId { get; set; }
+        public string locationName { get; set; }
+        public string locationPostcode { get; set; }
+        public Guid salesNumber { get; set; }
+        public double totalCost { get; set; }
+        public double totalTax { get; set; }
+
+        public class SaleEventDetail
+        {
+            public string productDescription { get; set; }
+            public Guid productId { get; set; }
+            public string productName { get; set; }
+            public int quantity { get; set; }
+            public double totalCost { get; set; }
+            public double totalTax { get; set; }
+            public double unitCost { get; set; }
+        }
+    }
+}
---
+++ BFYOC.Functions/SalesEventsProcessing.cs
@@ -0,0 +1,23 @@
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.ServiceBus;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BFYOC.Functions
+{
+    public static class SalesEventsProcessing
+    {
+        [FunctionName("SalesProcessing")]
+        public static async Task Run(
+            [EventHubTrigger("salesevents", Connection = "SalesEventHubConnection")]string[] salesEvents,
+            [DocumentDB(databaseName: "byoc", collectionName: "salesevents", ConnectionStringSetting = "CosmosDBConnection")] IAsyncCollector<string> salesEventsOut,
+            TraceWriter log)
+        {
+            log.Info($"Processing {salesEvents.Length} events");
+            var tasks = salesEvents.Select(e => salesEventsOut.AddAsync(e)).ToArray();
+            await Task.WhenAll(tasks);
+            log.Info($"Processing done");
+        }
+    }
+}
