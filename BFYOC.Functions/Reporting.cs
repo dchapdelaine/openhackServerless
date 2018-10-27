fa2eebd3674a48a0498f1193e781d09c0469f711
David Chapdelaine <david.chapdelaine@gmail.com>
2018-10-27 00:37:29 -0400

    work in progress for report generation

--- BFYOC.Functions/Reporting.cs
+++ BFYOC.Functions/Reporting.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -7,11 +8,32 @@ using Microsoft.Azure.Documents;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Azure.WebJobs.ServiceBus;
+using Microsoft.WindowsAzure.Storage;
 
 namespace BFYOC.Functions
 {
     public static class Reporting
     {
+        private static readonly Dictionary<string, string> mapping;
+        private static readonly CloudStorageAccount ReportStorageAccount = CloudStorageAccount.Parse(System.Environment.GetEnvironmentVariable("ReportsStorageConnection"));
+
+        static Reporting()
+        {
+            mapping = new Dictionary<string, string>
+            {
+                {"75542e38-563f-436f-adeb-f426f1dabb5c","Starfruit Explosion" },
+                {"e94d85bc-7bd0-44f3-854e-d8cd70348b63","Just Peachy" },
+                {"288fd748-ad2b-4417-83b9-7aa5be9cff22","Tropical Mango" },
+                {"76065ecd-8a14-426d-a4cd-abbde2acbb10","Gone Bananas" },
+                {"551a9be9-7f1c-447d-83ee-b18f5a6fb018","Matcha Green Tea" },
+                {"80bab959-ef8b-4ae3-8bf2-e876d77277b6","French Vanilla" },
+                {"4c25613a-a3c2-4ef3-8e02-9c335eb23204","Truly Orange-inal" },
+                {"65ab124a-9b2c-4294-a52d-18839364ef15","Durian Durian" },
+                {"e4e7068e-500e-4a00-8be4-630d4594735b","It's Grape!" },
+                {"0f5a0fe8-4506-4332-969e-699a693334a8","Beer" },
+            };
+        }
+
         [FunctionName("ReportGenerator")]
         public static async Task ReportGenerator([CosmosDBTrigger(
             databaseName: "byoc",
@@ -66,12 +88,59 @@ namespace BFYOC.Functions
         }
 
         [FunctionName("ReportingSales")]
-        public static async Task ReportingSales([EventHubTrigger("reports", Connection = "EventHubConnection")]string reportData,
+        public static async Task ReportingSales(
+            [EventHubTrigger("reports", Connection = "EventHubConnection")]string reportDataEvent,
             TraceWriter log)
         {
-            using (var csvReader = new CsvReader(new StringReader(reportData)))
+            var client = ReportStorageAccount.CreateCloudBlobClient();
+            var containerRef = client.GetContainerReference("reports");
+            using (var csvReader = new CsvReader(new StringReader(reportDataEvent)))
             {
+                var reportDataByTime = csvReader.GetRecords<dynamic>()
+                    .GroupBy(r => (string)r.time, r => new
+                    {
+                        Time = r.datetime,
+                        Score = r.score,
+                        TotalSales = r.totalcostsales,
+                        TotalOrders = r.totalcostorders,
+                        ProductId = r.productid
+                    });
+
+                foreach (var reportData in reportDataByTime)
+                {
+                    var reportSS = new StringWriter();
+                    reportSS.WriteLine($"Sales report for {reportData.Key}");
+
+                    reportSS.WriteLine();
+                    reportSS.WriteLine($"Sentiment score by ice cream");
+                    foreach (var orderedReportData in reportData.OrderByDescending(r => r.Score))
+                    {
+                        reportSS.WriteLine($"{orderedReportData.Score} - {ProductIdToProductName(orderedReportData.ProductId)}");
+                    }
+
+                    reportSS.WriteLine();
+                    reportSS.WriteLine($"Total orders in $ by ice cream");
+                    foreach (var orderedReportData in reportData.OrderByDescending(r => r.TotalOrders))
+                    {
+                        reportSS.WriteLine($"{orderedReportData.TotalOrders:C} - {ProductIdToProductName(orderedReportData.ProductId)}");
+                    }
+
+                    reportSS.WriteLine();
+                    reportSS.WriteLine($"Total sales events in $ by ice cream");
+                    foreach (var orderedReportData in reportData.OrderByDescending(r => r.TotalSales))
+                    {
+                        reportSS.WriteLine($"{orderedReportData.TotalSales:C} - {ProductIdToProductName(orderedReportData.ProductId)}");
+                    }
+
+                    var blob = containerRef.GetBlockBlobReference(DateTime.Parse(reportData.Key).ToString("YYYYMMDD_HHMMSS") + ".json");
+                    await blob.UploadTextAsync(reportSS.ToString());
+                }
             }
+        }
+
+        private static string ProductIdToProductName(string productId)
+        {
+            return mapping[productId];
         }
     }
 }
